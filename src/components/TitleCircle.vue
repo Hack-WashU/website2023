00bce3b78cad20ae7eb1d5d952998733a553aa94
Jack Heuberger <jackheuberger@wustl.edu>
2023-05-27 10:32:13 -0700

    created title circle

---
+++ src/components/TitleCircle.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+</script>
+
+<template>
+<div id="title_circle">
+    <h1 id="title">Hack WashU</h1>
+</div>
+</template>
+
+<style scoped>
+#title_circle {
+    position: relative;
+    width: 700px;
+    height: 700px;
+    margin-left: auto;
+    margin-right: auto;
+    border-radius: 50%; 
+    background: #C44444;
+}
+
+#title {
+    font-family: 'Russo One', sans-serif;
+    position: absolute;
+    width: 533px;
+    height: 308px;
+    left: 120px;
+    top: 150px;
+
+    font-family: 'Russo One';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 128px;
+    line-height: 131px;
+    text-align: right;
+
+    color: #FFFFFF;
+
+    text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    transform: rotate(-1.99deg);
+
+}
+</style>
