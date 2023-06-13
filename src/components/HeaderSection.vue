fe0cc81ba73a8fd2946b14389712475c6b5b7be7
emilyws27 <69002400+emilyws27@users.noreply.github.com>
2023-06-13 00:19:46 -0500

    grid

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -3,47 +3,152 @@ import TitleCircle from './TitleCircle.vue'
 </script>
 
 <template>
-<div class="header-section">    
-    <img id="planet13" src="../assets/custom-images/planet13.svg" alt="Bright yellow planet"/>
+    <div class="grid-container">
+        <!-- Repeat this grid-item as many times as you want rows -->
+        <div class="grid-item1">
+        </div>
+        <div class="grid-item2">
+        </div>
+        <div class="grid-item3">
+            <img id="planet3" src="../assets/custom-images/planet3.svg" alt="Small blue planet"/>
+
+        </div>
+        <div class="grid-item4"></div>
+        <div class="grid-item5">
+
+        </div>
+        <div class="grid-item6"></div>
+        <div class="grid-item7"></div>
+        <!-- ROW 2-->
+        <div class="grid-item1">
+        </div>
+        <div class="grid-item2">
+            <img id="planet8" src="../assets/custom-images/planet8.svg" alt="Orange planet"/>
+
+        </div>
+        <div class="grid-item3"></div>
+        <div class="grid-item4"></div>
+        <div class="grid-item5">
+
+        </div>
+        <div class="grid-item6"></div>
+        <div class="grid-item7"></div>
+                <!-- ROW 3-->
+
+        <div class="grid-item1">
+        </div>
+        <div class="grid-item2">
+        </div>
+        <div class="grid-item3"></div>
+        <div class="grid-item4"></div>
+        <div class="grid-item5">
+
+        </div>
+        <div class="grid-item6">
+
+        </div>
+
+        <div class="grid-item7">
+            <img id="planet2" src="../assets/custom-images/planet2.svg" alt="Pink and orange planet"/>
+
+        </div>
+                <!-- ROW 4-->
+
+        <div class="grid-item1">
+        </div>
+        <div class="grid-item2">
+        </div>
+        <div class="grid-item3"></div>
+        <div class="grid-item4"></div>
+        <div class="grid-item5">
+
+        </div>
+        <div class="grid-item6"></div>
+        <div class="grid-item7"></div>
+                <!-- ROW 5-->
+
+        <div class="grid-item1">
+        </div>
+        <div class="grid-item2">
+        </div>
+        <div class="grid-item3"></div>
+        <div class="grid-item4"></div>
+        <div class="grid-item5">
+
+        </div>
+        <div class="grid-item6"></div>
+        <div class="grid-item7"></div>
+                <!-- ROW 6-->
+
+        <div class="grid-item1">
+        </div>
+        <div class="grid-item2">
+        </div>
+        <div class="grid-item3"></div>
+        <div class="grid-item4"></div>
+        <div class="grid-item5">
+
+        </div>
+        <div class="grid-item6"></div>
+        <div class="grid-item7"></div>
+        <!--ROW 7-->
+        <div class="grid-item1">
+        </div>
+        <div class="grid-item2">
+        </div>
+        <div class="grid-item3"></div>
+        <div class="grid-item4"></div>
+        <div class="grid-item5">
+
+        </div>
+        <div class="grid-item6"></div>
+        <div class="grid-item7"></div>
+        <!--ROW 8-->
+        <div class="grid-item1">
+        </div>
+        <div class="grid-item2">
+        </div>
+        <div class="grid-item3"></div>
+        <div class="grid-item4"></div>
+        <div class="grid-item5">
+
+        </div>
+        <div class="grid-item6"></div>
+        <div class="grid-item7"></div>
+        <!--ROW 9-->
+        <div class="grid-item2"></div>
+        <div class="grid-item1"></div>
+        <div class="grid-item2"></div>
+        <!-- ... -->
+    </div>
+<div class="header-section">  
+
     <TitleCircle/>
-    <img id="planet3" src="../assets/custom-images/planet3.svg" alt="Small blue planet"/>
-    <img id="planet8" src="../assets/custom-images/planet8.svg" alt="Orange planet"/>
-    <img id="planet2" src="../assets/custom-images/planet2.svg" alt="Pink and orange planet"/>
 </div>
 </template>
 
 <style scoped>
-#planet2 {
-    position: absolute;
-    width: 199px;
-    height: 187px;
-    left: 1300px;
-    top: 533px;
+
+.grid-container {
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    grid-auto-rows: auto;
+    width: 100%;
+}
+.grid-item {
+border: 1px solid #ddd;
+padding: 10px;
+text-align: center;
+}
+.grid-item img {
+width: 100%;
+height: auto;
 }
 
-#planet8 {
-    position: absolute;
-    width: 247px;
-    height: 237px;
-    left: 250px;
-    top: 291px;
-}
 
-#planet3 {
-    position: absolute;
-    width: 99px;
-    height: 93px;
-    left: 365px;
-    top: 157px;
-}
 
-#planet13 {
-    /* box-sizing: border-box; */
-    position: absolute;
-    width: 146px;
-    height: 146px;
-    left: 1350px;
-    top: 50;
-    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-}
+
+
+
+
 </style>
