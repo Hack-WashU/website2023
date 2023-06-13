1627b89d0c216fc00d6f4288dc3abda9c6329ced
emilyws27 <69002400+emilyws27@users.noreply.github.com>
2023-06-13 01:00:53 -0500

    still going out of range

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -10,69 +10,69 @@
         <img src="../assets/custom-images/planet9.svg" alt="planet 9" id="planet9">
         <img src="../assets/custom-images/planet10.svg" alt="planet 10" id="planet10">
         <img src="../assets/custom-images/planet11.svg" alt="planet 11" id="planet11">
-    </div>
-        
+    </div>  
 </template>
 
 <style scoped>
 
 #planet4 {
     position: absolute;
-width: 199px;
-height: 187px;
-left: 1140px;
-top: 525px;
-
+    width: 199px;
+    height: 187px;
+    left: 1140px;
+    top: 525px;
 }
 #planet5 {
     position: absolute;
-width: 683px;
-height: 502px;
-left: 51px;
-top: 3630px;
+    width: 683px;
+    height: 502px;
+    left: 51px;
+    top: 3630px;
 }
 
 #planet7 {
     position: absolute;
-width: 249px;
-height: 233px;
-left: 300px;
-top: 3014px;
+    width: 249px;
+    height: 233px;
+    left: 300px;
+    top: 3014px;
 }
 #planet8 {
     position: absolute;
-width: 247px;
-height: 237px;
-left: 84px;
-top: 291px;
-
+    width: 247px;
+    height: 237px;
+    left: 84px;
+    top: 291px;
 }
 
 #planet9 {
     position: absolute;
-width: 600.17px;
-height: 500.15px;
-left: 641px;
-top: 1268.67px;
-
+    width: 600.17px;
+    height: 500.15px;
+    left: 641px;
+    top: 1268.67px;
 }
 #planet10 {
     position: absolute;
-width: 434px;
-height: 399px;
-left: 84px;
-top: 1543px;
+    width: 434px;
+    height: 399px;
+    left: 84px;
+    top: 1543px;
 }
 
 #planet11{
     position: absolute;
-width: 296px;
-height: 296px;
-left: 1088px;
-top: 4182px;
+    width: 296px;
+    height: 296px;
+    left: 1088px;
+    top: 4182px;
 }
 
-
-
+.parent {
+    position: relative;
+    width: 100vw; /* take full viewport width */
+    height: 100vh; /* take full viewport height */
+    overflow: hidden; /* hide overflowing content */
+}
 
 </style>
