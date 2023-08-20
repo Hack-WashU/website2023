41df4ae016ceb727305354e150f83deccee92fc0
Jack Heuberger <jackheuberger@wustl.edu>
2023-08-19 19:51:23 -0500

    replace gtag

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import { createApp } from 'vue'
 import './assets/reset.css'
 import App from './App.vue'
-import VueGtag from "vue-gtag";
 
-createApp(App).use(VueGtag, {
-    config: { id: 'G-SD3BFH9QTK' }
-}).mount('#app')
+createApp(App).mount('#app')
